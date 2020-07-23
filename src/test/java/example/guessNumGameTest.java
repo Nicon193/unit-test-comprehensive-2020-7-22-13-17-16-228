339c6df80722ad72972b79e14fa24ff8e0f8b9de
Albert <Albert.c.x.chen@oocl.com>
2020-07-23 22:49:40 +0800

    [Albert] feature: complete should_return_0A4B_when_guessNum_given_answer_is_1234_guess_is_4321 test

--- src/test/java/example/guessNumGameTest.java
+++ src/test/java/example/guessNumGameTest.java
@@ -19,4 +19,20 @@ public class guessNumGameTest {
         //then
         assertEquals("4A0B",actual);
     }
+
+    @Test
+    void should_return_0A4B_when_guessNum_given_answer_is_1234_guess_is_4321() {
+        //given
+        String actual;
+        String answer ="1234";
+        String guess ="4321";
+
+        //when
+        guessNumGame gNG =new guessNumGame(answer);
+        actual=gNG.guessNum(guess);
+
+        //then
+        assertEquals("0A4B",actual);
+    }
+
 }
