2b06f5fa6e437c7281dfea019f1dca02b66c68c2
Albert <Albert.c.x.chen@oocl.com>
2020-07-23 23:40:41 +0800

    [Albert] refactor: refactor refactor mention

--- src/main/java/example/guessNumGame.java
+++ src/main/java/example/guessNumGame.java
@@ -11,28 +11,37 @@ public class guessNumGame {
     }
 
     public String guessNum(String guess) {
-         int a=0;
-         int b=0;
+
+
+        int a=countTotalCorrectNum(guess);
+        int b=countExistNum(guess);
+
+        return String.format(result,a,b-a);
+    }
+
+    public  int countTotalCorrectNum(String guess){
+        int a=0;
+        for (int i = 0; i <answer.length() ; i++) {
+            if (answer.charAt(i)==guess.charAt(i)){
+                a++;
+            }
+        }
+        return a;
+    }
+
+    public int countExistNum(String guess){
+        int b=0;
         HashSet<Character> set= new HashSet<>();
 
-         for(char c : answer.toCharArray()){
-             set.add(c);
-         }
+        for(char c : answer.toCharArray()){
+            set.add(c);
+        }
 
         for (int i = 0; i <guess.length() ; i++) {
             if (!set.add(guess.charAt(i))){
                 b++;
             }
         }
-
-
-
-        for (int i = 0; i <answer.length() ; i++) {
-            if (answer.charAt(i)==guess.charAt(i)){
-                a++;
-            }
-        }
-
-            return String.format(result,a,b-a);
+        return b;
     }
 }
