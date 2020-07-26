7eff6bcc1de79bcfe302b117fad973b5c76c82a7
Albert <Albert.c.x.chen@oocl.com>
2020-07-26 18:23:06 +0800

    [Albert] feature: compelete playGame class

---
+++ src/main/java/example/playGame.java
@@ -0,0 +1,46 @@
+package example;
+
+import java.util.Scanner;
+
+public class playGame {
+    public static void main(String[] args) {
+        int times =6;
+        generateAnswerByRandom randomAnswer = new generateAnswerByRandom();
+        String answer =randomAnswer.generateAnswer();
+        isNumVaild isNumVaild =new isNumVaild();
+
+//        System.out.println("answer is "+answer);
+        guessNumGame guessNumGame =new guessNumGame(answer);
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Game Start");
+
+        while (scan.hasNextLine()&&times>=1) {
+            String inputLine = scan.nextLine();
+            String inputNum=inputLine.replace(" ","");
+           // System.out.println(inputNum);
+            if(!isNumVaild.isLegalNum(inputNum)){
+                System.out.println("Wrong Input，Input again");
+                times--;
+            }else {
+                String output =guessNumGame.guessNum(inputNum);
+                System.out.println(output);
+                if (output.equals("4A0B")){
+                    System.out.print("you win ");
+                    break;
+
+                }
+                times--;
+
+            }
+
+
+
+        }
+
+        System.out.println(("answer is "+answer));
+
+    }
+}
