f8b3cca88d347d1d957996299dc4374a870d21b5
Albert <Albert.c.x.chen@oocl.com>
2020-07-26 18:31:12 +0800

    [Albert] refactor: refactor playGame class

--- src/main/java/example/playGame.java
+++ src/main/java/example/playGame.java
@@ -4,43 +4,38 @@ import java.util.Scanner;
 
 public class playGame {
     public static void main(String[] args) {
-        int times =6;
+        int times = 6;
         generateAnswerByRandom randomAnswer = new generateAnswerByRandom();
-        String answer =randomAnswer.generateAnswer();
-        isNumVaild isNumVaild =new isNumVaild();
+        String answer = randomAnswer.generateAnswer();
+        isNumVaild isNumVaild = new isNumVaild();
 
 //        System.out.println("answer is "+answer);
-        guessNumGame guessNumGame =new guessNumGame(answer);
-
-
+        guessNumGame guessNumGame = new guessNumGame(answer);
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Game Start");
 
-        while (scan.hasNextLine()&&times>=1) {
+        while (times >= 1 && scan.hasNextLine()) {
             String inputLine = scan.nextLine();
-            String inputNum=inputLine.replace(" ","");
-           // System.out.println(inputNum);
-            if(!isNumVaild.isLegalNum(inputNum)){
+            String inputNum = inputLine.replace(" ", "");
+            // System.out.println(inputNum);
+            if (!isNumVaild.isLegalNum(inputNum)) {
                 System.out.println("Wrong Input，Input again");
                 times--;
-            }else {
-                String output =guessNumGame.guessNum(inputNum);
+            } else {
+                String output = guessNumGame.guessNum(inputNum);
                 System.out.println(output);
-                if (output.equals("4A0B")){
+                if (output.equals("4A0B")) {
                     System.out.print("you win ");
                     break;
-
                 }
                 times--;
 
             }
 
-
-
         }
 
-        System.out.println(("answer is "+answer));
+        System.out.println(("answer is " + answer));
 
     }
 }
