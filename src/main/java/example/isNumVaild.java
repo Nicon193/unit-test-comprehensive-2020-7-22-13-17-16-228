5763b2886ae07e9f7790d32522ebafa0209d6fa9
Albert <Albert.c.x.chen@oocl.com>
2020-07-28 12:55:46 +0800

    [Albert] refactor: extract "[0-9]{4}" store in FOUR_NUMBER_PATTERN

--- src/main/java/example/isNumVaild.java
+++ src/main/java/example/isNumVaild.java
@@ -4,21 +4,20 @@ import java.util.HashSet;
 import java.util.regex.Pattern;
 
 public class isNumVaild {
+
+    private final String FOUR_NUMBER_PATTERN = "[0-9]{4}";
+
     public boolean isLegalNum(String input) {
         boolean isVaild ;
         boolean isFourNum;
         boolean isNotRepeat;
         HashSet<Character> set = new HashSet<>();
-        Pattern pattern =Pattern.compile("[0-9]{4}");
-        generateAnswerByRandom randomAnswer =new generateAnswerByRandom();
-
-
+        Pattern pattern =Pattern.compile(FOUR_NUMBER_PATTERN);
 
         isFourNum =pattern.matcher(input).matches();
 
         for (int i = 0; i <input.length() ; i++) {
             set.add(input.charAt(i));
-
         }
 
         isNotRepeat= set.size() == input.length();
