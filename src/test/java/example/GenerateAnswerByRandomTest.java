78657eec65c911e2e3b0403614b600947b9b52d1
Albert <Albert.c.x.chen@oocl.com>
2020-07-28 13:15:15 +0800

    [Albert] refactor: rename generateAnswerByRandomTest to GenerateAnswerByRandomTest

--- src/test/java/example/GenerateAnswerByRandomTest.java
+++ src/test/java/example/GenerateAnswerByRandomTest.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class generateAnswerByRandomTest {
+public class GenerateAnswerByRandomTest {
     @Test
     void isVaildAnswer() {
 
